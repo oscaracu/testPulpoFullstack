8137ba0ac07cdfa37f6e86ad3359e1e4d2a897da
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-01-04 04:17:45 +0000

    Initial Commit

---
+++ pulpo-backend/src/vehicles/vehicles.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+
+@Injectable()
+export class VehiclesService {
+  create(createVehicleDto: CreateVehicleDto) {
+    return 'This action adds a new vehicle';
+  }
+
+  findAll() {
+    return `This action returns all vehicles`;
+  }
+
+  findNovelties() {
+    return 'This action returns all novelties';
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} vehicle`;
+  }
+
+  update(id: number, updateVehicleDto: UpdateVehicleDto) {
+    return `This action updates a #${id} vehicle`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} vehicle`;
+  }
+}
