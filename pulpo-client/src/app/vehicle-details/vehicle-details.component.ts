4f23cb5fce37b961e32c522bb489f959fa941202
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-01-17 20:37:25 +0000

    frontend/vehicle-details/modal

--- pulpo-client/src/app/vehicle-details/vehicle-details.component.ts
+++ pulpo-client/src/app/vehicle-details/vehicle-details.component.ts
@@ -14,6 +14,7 @@ import { Vehicle } from '../vehicle';
 export class VehicleDetailsComponent implements OnInit {
   vehicle!: Vehicle;
   noveltiesCategories!: NoveltiesCategories[];
+  delete: boolean = true;
 
   newNovelty = new FormGroup({
     noveltiesCategoryId: new FormControl(0, { nonNullable: true }),
