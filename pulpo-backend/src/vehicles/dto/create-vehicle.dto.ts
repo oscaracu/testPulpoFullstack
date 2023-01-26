808bcfc287ec5ecd2f7c81081b867213f20cd00b
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-01-26 12:11:00 +0000

    backend/vehicle-validators-to-dto

--- pulpo-backend/src/vehicles/dto/create-vehicle.dto.ts
+++ pulpo-backend/src/vehicles/dto/create-vehicle.dto.ts
@@ -1,13 +1,25 @@
-import { Color } from "src/colors/entities/color.entity";
-import { Maker } from "src/makers/entities/maker.entity";
-import { Novelty } from "src/novelties/entities/novelty.entity";
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Color } from 'src/colors/entities/color.entity';
+import { Maker } from 'src/makers/entities/maker.entity';
+import { Novelty } from 'src/novelties/entities/novelty.entity';
 
 export class CreateVehicleDto {
-    make: Maker;
-    model: number;
-    color: Color;
-    admissionDate: string;
-    isActive: boolean;
-    isAssigned: boolean;
-    novelties?: Novelty[];
+  @IsNotEmpty()
+  @IsNumber()
+  make: Maker;
+
+  @IsNotEmpty()
+  @IsNumber()
+  model: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  color: Color;
+
+  @IsNotEmpty()
+  @IsString()
+  admissionDate: string;
+  isActive: boolean;
+  isAssigned: boolean;
+  novelties?: Novelty[];
 }
