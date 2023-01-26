cd1e0384d9e143c8e1f198ddfbcbe89a1482624e
Oscar del Angel Cu Reyes <oscaracu@miplazaweb.com.mx>
2023-01-26 13:00:11 +0000

    backend/novelties-validators

--- pulpo-backend/src/novelties/dto/create-novelty.dto.ts
+++ pulpo-backend/src/novelties/dto/create-novelty.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsNumber } from 'class-validator';
+
 export class CreateNoveltyDto {
+  @IsNotEmpty()
+  @IsNumber()
   noveltiesCategoryId: number;
+  @IsNotEmpty()
+  @IsNumber()
   description: string;
 }
